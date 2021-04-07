5b07a85abeb535dffe83083cc788c93e7113d23b
Yoseph Parulian <yoseph.tdf@gmail.com>
2021-04-07 18:45:34 +0700

    make it better

---
+++ resources/views/pages/product-galleries/index.blade.php
@@ -0,0 +1,56 @@
+@extends('layouts.default')
+@section('content')
+    <div class="orders">
+        <div class="row">
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="box-title">Daftar Foto Barang</div>
+                    </div>
+                    <div class="card-body">
+                        <div class="table-stats order-table ov-h">
+                            <table class="table">
+                                <thead>
+                                    <tr>
+                                        <th>#</th>
+                                        <th>Nama Barang</th>
+                                        <th>Foto</th>
+                                        <th>Default</th>
+                                        <th>Action</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    @forelse ($items as $item)
+                                    <tr>
+                                        <td>{{ $loop->iteration }}</td>
+                                        <td>{{ $item->product->name }}</td>
+                                        <td>
+                                            <img src="{{ url($item->photo) }}" alt="">
+                                        </td>
+                                        <td>{{ $item->default ? 'Ya' : 'Tidak' }}</td>
+                                        <td>
+                                            <form action="{{ route('product-galleries.destroy', $item->id) }}" method="POST" class="d-inline">
+                                                @method('delete')
+                                                @csrf
+                                                <button class="btn btn-danger btn-sm">
+                                                    <i class="fa fa-trash"></i>
+                                                </button>
+                                            </form>
+                                        </td>
+                                    </tr>
+                                    @empty
+                                        <tr>
+                                            <td class="text-center" colspan="6">
+                                             Data Tidak Ditemukan.
+                                            </td>
+                                        </tr>
+                                    @endforelse
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
